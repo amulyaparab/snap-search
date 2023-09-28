f4538375242da6673debf4f0a3a89dd57fbdc912
Amulya Parab <amulyaparab101@gmail.com>
2023-09-28 18:44:26 +0530

    feat: click outside functionality & responsiveness

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,44 +1,57 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DropDown from "./Dropdown";
 import data from "../database/data";
 
 const SearchBar = ({ closeSearch }) => {
   const inputRef = useRef(null);
+  const searchRef = useRef(null);
 
   const [searchText, setSearchText] = useState("");
   const [showDropDown, setShowDropDown] = useState(false);
 
+  const showTheDropdown = () => setShowDropDown(true);
+  const hideTheDropdown = () => setShowDropDown(false);
+
   const findValueInData = data.filter(({ feature }) =>
     feature.toLowerCase().includes(searchText?.toLowerCase()?.trim())
   );
-
-  const showTheDropdown = () => setShowDropDown(true);
 
   const searchValue = (text) => {
     setSearchText(text);
     setShowDropDown(false);
   };
 
-  const focusInput = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  };
+  const focusInput = () => inputRef.current && inputRef.current.focus();
 
   const clearSearch = () => {
     setSearchText("");
     focusInput();
   };
 
+  const handleClickOutside = (event) => {
+    const elementIsNotInside = !searchRef.current.contains(event.target);
+    if (searchRef.current && elementIsNotInside) {
+      hideTheDropdown();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative">
+    <div className="relative" ref={searchRef}>
       <i
         className="fa-solid fa-circle-xmark search-icon close"
         onClick={closeSearch}
       ></i>
       <input
         ref={inputRef}
-        placeholder="Search"
+        placeholder="Search here"
         className="search-bar"
         value={searchText}
         onChange={(event) => setSearchText(event.target.value)}
